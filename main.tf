3df6b66c73686a91f1ca8e82eac6622cd35d2c7d
bharatcharan <129725236+bharatcharan@users.noreply.github.com>
2024-05-04 09:53:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -353,8 +353,6 @@ output "lb_dns_name" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "aelugu"
-  acl    = "private"
-  # Other bucket configurations...
 }
 resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   bucket = aws_s3_bucket.my_bucket.id
